178d00d79ba0190db819df4fae46225db8295f38
vasjabot <vasjabot88@gmail.com>
2019-01-26 15:03:09 +0300

    update_craft.php was added

---
+++ update_craft.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("esfull:craftUpdateFull");
+?><?$APPLICATION->IncludeComponent(
+	"esfull:craftUpdateFull",
+	"",
+Array()
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

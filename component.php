9a51645682a7ee4876c09a0b81beb14dd6d6149a
vasjabot <vasjabot88@gmail.com>
2019-01-27 14:37:21 +0300

    getAllXML was added

--- component.php
+++ component.php
@@ -6,63 +6,26 @@ require_once(__DIR__.'/include.php');
 $DEBUG = TRUE;
 
 $config = new CommonNS\AppConfig();
-print_php($config->auth_path_file);
-//$url = $config->getURLbyFileName("prototypes_work.xml");
-//print_php($url);
-//print_php($config->LOGIN);
-//print_php($config->PWS);
 
 
+$XML_arr = array  (	"prototypes" => $xml_prototypes,
+	             	"offers" => $xml_offers,
+	               	"prices" => $xml_prices,
+	               	"instock" => $xml_instock,
+	               	"compatibility" => $xml_compatibility);
 
 
-$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider('prototypes_work.xml', $config, $DEBUG);
-$xml_prototypes = $simpleXMLprovider->getFileXML('prototypes_work.xml');
+$XML_arr_names = array(	"prototypes" => 'prototypes_work.xml',
+	               		"offers" => 'offers_work.xml',
+	               		"prices" => 'prices_work.xml',
+	               		"instock" => 'instock_work.xml',
+	               		"compatibility" => 'compatibility_work.xml');
 
-$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider('offers_work.xml', $config, $DEBUG);
-$xml_offers = $simpleXMLprovider->getFileXML('offers_work.xml');
+$XML_arr = getAllXML($XML_arr_names, $config, $DEBUG);
 
-
-$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider('prices_work.xml', $config, $DEBUG);
-$xml_prices = $simpleXMLprovider->getFileXML('prices_work.xml');
-
-
-$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider('instock_work.xml', $config, $DEBUG);
-$xml_instock = $simpleXMLprovider->getFileXML('instock_work.xml');
+printAllXML($XML_arr);
 
 
-$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider('compatibility_work.xml', $config, $DEBUG);
-$xml_compatibility = $simpleXMLprovider->getFileXML('compatibility_work.xml');
-
-
-$XML_arr_names = array  (	"prototypes" => 'prototypes_work.xml',
-		               		"offers" => 'offers_work.xml',
-		               		"prices" => 'prices_work.xml',
-		               		"instock" => 'instock_work.xml',
-		               		"compatibility" => 'compatibility_work.xml');
-
-
-$simpleXMLvalidator = new SimpleXMLNS\SimpleXMLvalidator($config);
-
-
-
-$Is_All_Good = $simpleXMLvalidator->checkAllXML($XML_arr_names);
-
-if ($Is_All_Good == FALSE)
-{
-   return;
-}
-
-
-
-
-
-
-
-print_php($xml_prototypes);
-print_php($xml_offers);
-print_php($xml_prices);
-print_php($xml_instock);
-print_php($xml_compatibility);
 
 
 
--- include.php
+++ include.php
@@ -14,6 +14,46 @@ require_once(__DIR__.'/classes/SimpleXML/SimpleXMLvalidator.php');
 
 
 
+function getAllXML($XML_arr_names, $config, $DEBUG) 
+{
+	$XML_arr = array  (	"prototypes" => $xml_prototypes,
+		               	"offers" => $xml_offers,
+		               	"prices" => $xml_prices,
+		               	"instock" => $xml_instock,
+		               	"compatibility" => $xml_compatibility);
+
+
+	foreach($XML_arr_names as $key => $value)
+    {
+       	$simpleXMLprovider = new SimpleXMLNS\SimpleXMLprovider($value, $config, $DEBUG);
+		$xml_temp_result = $simpleXMLprovider->getFileXML($value);
+		$XML_arr[$key] = $xml_temp_result;
+    } 
+    
+    $simpleXMLvalidator = new SimpleXMLNS\SimpleXMLvalidator($config);
+
+	$Is_All_Good = $simpleXMLvalidator->checkAllXML($XML_arr_names);
+
+	if ($Is_All_Good == FALSE)
+	{
+	   return NULL;
+	}
+	else
+	{
+		return $XML_arr;
+	}
+}
+
+function printAllXML($XML_arr) 
+{	
+	foreach($XML_arr as $key => $value)
+	{
+		print_php($value);
+	}
+
+}
+
+
 
 set_time_limit(0);
 $t0 = microtime(true);

0776df638baefcaf9bab5e67adc69d3f9e6cf978
vasjabot <vasjabot88@gmail.com>
2019-01-30 16:43:22 +0300

    SimpleXMLprovider was refactor and include.php was cleared

--- classes/SimpleXML/SimpleXMLprovider.php
+++ classes/SimpleXML/SimpleXMLprovider.php
@@ -23,19 +23,18 @@ require_once(__DIR__.'/SimpleXMLloader.php');
 interface SimpleXMLproviderInterface
 {
     ///return $result_xml///
-    public function getFileXML();
-    public function requestNewXML();
+    public function getFileXML($XML_name);
+    public function requestNewXML($XML_name);
+    public function getAllXML($XML_arr_names)
 }
 
 abstract class AbstractSimpleXMLprovider implements SimpleXMLproviderInterface
 {
-    protected $file_name;
     protected $config;
     protected $DEBUG;
 
-    public function __construct($FileName, $Config, $DEBUG=FALSE)
+    public function __construct($Config, $DEBUG=FALSE)
     {
-        $this->file_name = $FileName;
         $this->config = $Config;
         $this->DEBUG = $DEBUG;      
     }
@@ -43,35 +42,70 @@ abstract class AbstractSimpleXMLprovider implements SimpleXMLproviderInterface
 
 class SimpleXMLprovider extends AbstractSimpleXMLprovider
 {
-    public function getFileXML()
+    public function getFileXML($XML_name)
     {
         if ($this->DEBUG == TRUE) 
         {
             $simpleXMLloader = new SimpleXMLloader($this->config);
-            $xml_return = $simpleXMLloader->loadFileXML($this->file_name);
+            $xml_return = $simpleXMLloader->loadFileXML($XML_name);
             if ($xml_return == NULL)
             {
-                $xml_return = $this->requestNewXML();    
+                $xml_return = $this->requestNewXML($XML_name);    
             }
             return $xml_return;
         }
         else
         {
-            return $xml_return = $this->requestNewXML();
+            return $xml_return = $this->requestNewXML($XML_name);
         }      
     }
 
 
-    public function requestNewXML()
+    public function requestNewXML($XML_name)
     {
         $curlRequestor = new CurlRequestor($this->config->LOGIN, $this->config->PWS);
-        $url = $this->config->getURLbyFileName($this->file_name);
+        $url = $this->config->getURLbyFileName($XML_name);
         $result_request = $curlRequestor->getData($url);
 
         $simpleXMLsaver = new SimpleXMLsaver($this->config);
-        $xml_request = $simpleXMLsaver->saveFileXML($this->file_name, $result_request);
+        $xml_request = $simpleXMLsaver->saveFileXML($XML_name, $result_request);
         return $xml_request;
     }
+
+
+
+
+    public function getAllXML($XML_arr_names)
+    {
+        $XML_arr = array  ( "prototypes" => $xml_prototypes,
+                            "offers" => $xml_offers,
+                            "prices" => $xml_prices,
+                            "instock" => $xml_instock,
+                            "compatibility" => $xml_compatibility);
+
+
+        foreach($XML_arr_names as $key => $value)
+        {
+            $xml_temp_result = $this->getFileXML($value);
+            $XML_arr[$key] = $xml_temp_result;
+        } 
+        
+        $simpleXMLvalidator = new SimpleXMLNS\SimpleXMLvalidator($config);
+
+        $Is_All_Good = $simpleXMLvalidator->checkAllXML($XML_arr_names);
+
+        if ($Is_All_Good == FALSE)
+        {
+           return NULL;
+        }
+        else
+        {
+            return $XML_arr;
+        }
+
+
+    }
+
 
 
 }

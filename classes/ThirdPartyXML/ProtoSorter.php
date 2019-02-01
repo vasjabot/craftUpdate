7b2b2a859e3ae523aa2c98255bd050a99a9abd3a
vasjabot <vasjabot88@gmail.com>
2019-02-01 11:48:19 +0300

    classes/ThirdPartyXML/ProtoSorter.php was cleared

--- classes/ThirdPartyXML/ProtoSorter.php
+++ classes/ThirdPartyXML/ProtoSorter.php
@@ -42,7 +42,6 @@ class ProtoSorter extends AbstractProtoSorter
 
         if ($xlsx = SimpleXLSX::parse($PathToXlsx)) 
         {
-            //$xlsx = mb_convert_encoding($xlsx, "utf-8", "windows-1251");
             $rows = $xlsx->rows(3);
             $protoGetterSite = new ProtoGetterSite($this->config); 
             $allSection = $protoGetterSite->getArrayAllSection();
@@ -53,19 +52,8 @@ class ProtoSorter extends AbstractProtoSorter
 
             foreach ($allSection as $key => $value)
             {
-                // print_r($key);
-                // echo nl2br("\r\n");
-                // print_r($value);
-                // echo nl2br("\r\n");
-
                 foreach ($value as $key_in => $value_in)
                 {
-                    // print_r($key_in);
-                    // echo nl2br("\r\n");
-                    // print_r($value_in);
-                    // echo nl2br("\r\n");
-                    //break;
-
 
                     if($key_in == "CODE") 
                     {
@@ -73,138 +61,52 @@ class ProtoSorter extends AbstractProtoSorter
                         $value_in = str_replace('_', '.', $value_in);
                         $value_in = str_replace('_', '/', $value_in);
 
-    
                         $result_allSection[] = $value_in;
-                        // print_r($key);
-                        // echo nl2br("\r\n");
-                        // print_r($value);
-                        // echo nl2br("\r\n");
-                        //if ($value_in == "acer liquid z530s")
-                        //{
-                            //print_r("acer liquid z530s WAS FOUND!!!!!!!!!");
-                        //}
                     }
 
-                }
+                }         
 
+            }  
                 
+            $return_array = array();
 
-            }
-
-
-            foreach ($result_allSection as $item)
+            foreach ($rows as $key => $value)
             {
-                //print_r($item);
-                //echo nl2br("\r\n");
-            }
-
-
-
-
-
-            $i=0;
-            if(1)
-            {    
-                $return_array = array();
-
-                foreach ($rows as $key => $value)
+                foreach ($result_allSection as $secItemCode)
                 {
-                    //break;
-                    //// $rows[$key] = iconv("UTF-8", "CP1251",$value);
-                    //// $rows[$key] = iconv("windows-1251", "utf-8",$value);
 
+                    $pos = strpos($value[0], $secItemCode);
 
-                    // print_r($key);
-                    // echo nl2br("\r\n");
-                    //print_r($value[1]);
-                    //echo nl2br("\r\n");
-
-                    foreach ($result_allSection as $secItemCode)
+                    if ($pos === false) 
                     {
-                         // print_r($secItemCode);
-                         // echo nl2br("\r\n");
-                     
-                        $pos = strpos($value[0], $secItemCode);
-
-                        if ($pos === false) 
+                        continue;
+                       
+                    } else 
+                    {
+                        if ($prev_value_1 == $value[1])
                         {
                             continue;
-                           
-                        } else 
-                        {
-                            if ($prev_value_1 == $value[1])
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                $return_array[$secItemCode] += $value[1];
-                                // echo "String WAS found";
-                                // print_r($value[0]);
-                                // echo nl2br("\r\n");
-                                // print_r($value[1]);
-                                // echo nl2br("\r\n");
-
-                                $prev_value_1 = $value[1];
-
-                                break;
-
-                            }                                                  
                         }
-                    } 
-
-                    $i++;
-                    if ($i>4000)
-                    {
-                        break;
+                        else
+                        {
+                            $return_array[$secItemCode] += $value[1];
+                            $prev_value_1 = $value[1];
+                            break;
+                        }                                                  
                     }
-
-
-                   
-
-
-                        
-
-                    
-                }
-
-                //$number_array = array();
-
-                foreach ($return_array as $key => $value)
-                {
-                    print_r($key);
-                    echo nl2br("\r\n");
-                    print_r($value);
-                    echo nl2br("\r\n");
-                    // if ($value > 10000)
-                    // {
-                    //     $return_array[$key] = 10000;
-                    // }
-                    //$number_array[] = $value;
-
-                }
-
-                //sort($number_array);
-
-                // $i = 0;
-                // foreach ($number_array as $number)
-                // {
-                //     $i++;
-                //     print_r($number);
-                //     echo nl2br("\r\n");
-
-                // }
-
-                // print_r("i " . $i);
-                // echo nl2br("\r\n");
-
-
-    
-
-
+                }               
             }
 
-            //print_r($rows);
+            foreach ($return_array as $key => $value)
+            {
+                print_r($key);
+                echo nl2br("\r\n");
+                print_r($value);
+                echo nl2br("\r\n");
+            }
+
+            
+
         } else
         {
             echo SimpleXLSX::parseError();
